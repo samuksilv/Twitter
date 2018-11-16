04c32dd454801a7ed5da60fbdd716a02c613616d
Samuel Silva De Carvalho <samuel.carvalho@consinco.com.br>
2018-11-16 16:24:06 -0200

    criação de arquivos docker file e docker compose para subir a aplicação em containers

--- WebClient/src/components/Tweet.js
+++ WebClient/src/components/Tweet.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
-
-// import { Container } from './styles';
-
+import LikeImg from '../assets/like.svg'
+import './Tweet.css';
+import api from '../services/api';
 export default class Tweet extends Component {
+  handleLike= ()=>{
+    
+  };
   render() {
-    let {tweet}= this.props;
+    let {tweet}= this.props;    
 
     return(
         <li className='tweet'>
             <strong>{tweet.author}</strong>
+            <p>{tweet.content}</p>
+            <button onClick={this.handleLike} >
+              <img src={LikeImg}/>
+              <p>{tweet.likes}</p>
+            </button>
         </li>
     );
   }
